004ca1289b595baf4e87958c10cb4b42a2e8bf75
crynxmartinez <crynxmartinez@github.com>
2025-11-27 20:30:36 +0800

    Hide sidebar on login, register, forgot-password pages

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -6,6 +6,8 @@ import MobileNav from './MobileNav'
 export default function Layout() {
   const location = useLocation()
   const isHomePage = location.pathname === '/'
+  const isAuthPage = ['/login', '/register', '/forgot-password'].includes(location.pathname)
+  const hideSidebar = isHomePage || isAuthPage
 
   return (
     <div className="min-h-screen bg-dark-950">
@@ -18,11 +20,11 @@ export default function Layout() {
       {/* Main Content */}
       <div className="flex">
         {/* Sidebar (Desktop) - Hidden on home page */}
-        {!isHomePage && <Sidebar />}
+        {!hideSidebar && <Sidebar />}
 
         {/* Page Content */}
         <main className="flex-1 min-h-[calc(100vh-4rem)]">
-          <div className={`mx-auto px-4 sm:px-6 lg:px-8 py-6 ${isHomePage ? 'max-w-7xl' : 'max-w-6xl'}`}>
+          <div className={`mx-auto px-4 sm:px-6 lg:px-8 py-6 ${hideSidebar ? 'max-w-7xl' : 'max-w-6xl'}`}>
             <Outlet />
           </div>
         </main>
