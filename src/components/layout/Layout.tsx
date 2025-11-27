39b948313df4876e92b5a10a0f081259d0b837a6
crynxmartinez <crynxmartinez@github.com>
2025-11-27 20:24:07 +0800

    Hide sidebar on home page

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,12 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Navbar from './Navbar'
 import Sidebar from './Sidebar'
 import MobileNav from './MobileNav'
 
 export default function Layout() {
+  const location = useLocation()
+  const isHomePage = location.pathname === '/'
+
   return (
     <div className="min-h-screen bg-dark-950">
       {/* Navbar */}
@@ -14,12 +17,12 @@ export default function Layout() {
 
       {/* Main Content */}
       <div className="flex">
-        {/* Sidebar (Desktop) */}
-        <Sidebar />
+        {/* Sidebar (Desktop) - Hidden on home page */}
+        {!isHomePage && <Sidebar />}
 
         {/* Page Content */}
         <main className="flex-1 min-h-[calc(100vh-4rem)]">
-          <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+          <div className={`mx-auto px-4 sm:px-6 lg:px-8 py-6 ${isHomePage ? 'max-w-7xl' : 'max-w-6xl'}`}>
             <Outlet />
           </div>
         </main>
